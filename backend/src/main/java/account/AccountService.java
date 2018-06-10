a0a0e4e02dfc7f116e7ead0edd7c0eb07571e5dc
Iuliia Mykhailovych <jmykhailovych@gmail.com>
2018-06-10 21:41:33 +0200

    backend basic implementation

---
+++ backend/src/main/java/account/AccountService.java
@@ -0,0 +1,17 @@
+package account;
+
+import java.util.List;
+
+public interface AccountService {
+
+  public Account getAccountById(Long id);
+
+  public List<Account> getAccounts();
+
+  public Account addAccount(Account account);
+
+  public void deleteAccount(Long id, Account account);
+
+  public void deleteAccount(Long id);
+
+}
