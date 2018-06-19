f02523a807edbd0ec86c47ec888fc0e491e5c4d9
SebaP <spacuk@sigma.ug.edu.pl>
2018-06-19 11:30:21 +0200

    added @croosOrigin

--- backend/src/main/java/com/personalfinancemanager/controllers/AccountController.java
+++ backend/src/main/java/com/personalfinancemanager/controllers/AccountController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -24,6 +25,7 @@ import java.util.List;
 @AllArgsConstructor
 @RestController
 @RequestMapping(value = "${rest.mapping.accountController}")
+@CrossOrigin(origins = "http://localhost:4200")
 public class AccountController{
 
   @Autowired
