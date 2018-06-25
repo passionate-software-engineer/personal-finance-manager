51dc9a023421105b3981b93b3f7d2a76faddaf3b
piotrhawlicki <piotrek.hawlicki@gmail.com>
2018-06-25 20:52:49 +0200

    test for getAccountbyID

--- backend/src/main/java/com/pfm/controllers/AccountControllerTest.java
+++ backend/src/main/java/com/pfm/controllers/AccountControllerTest.java
@@ -3,25 +3,37 @@ package com.pfm.controllers;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.ResultMatcher;
 
-//@RunWith(SpringRunner.class)
-//@SpringBootTest
-//@AutoConfigureMockMvc
+import static org.springframework.mock.http.server.reactive.MockServerHttpRequest.get;
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.content;
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.hamcrest.Matchers.is;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+
 public class AccountControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
 
     @Test
-    public void getAccountById() {
-        String accountJson = "{\"id\":1,\"name\":\"Piotrek\",\"balance\":1000}";
+    public void getAccountById() throws Exception {
 
         this.mockMvc
-                .perform(get("/accounts/1")
-                .contentType("application/json;charset=UTF-8"))
+                .perform((RequestBuilder) get("/accounts/1"))
+                .andExpect((ResultMatcher) content().contentType("application/json;charset=UTF-8"))
                 .andDo(print()).andExpect(status().isOk())
-                .andExpect(jsonPath("$.id", is(1)));
-
+                .andExpect((ResultMatcher) jsonPath("$.id", is(1)));
     }
 
     @org.junit.Test
