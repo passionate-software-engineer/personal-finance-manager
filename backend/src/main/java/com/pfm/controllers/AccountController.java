69d8a46a38cb336a073f7bd6e40e8325ab93e274
piotrhawlicki <piotrek.hawlicki@gmail.com>
2018-06-25 20:52:49 +0200

    test for getAccountbyID

--- backend/src/main/java/com/pfm/controllers/AccountController.java
+++ backend/src/main/java/com/pfm/controllers/AccountController.java
@@ -7,15 +7,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -31,6 +23,7 @@ public class AccountController {
   private AccountService accountService;
 
   @GetMapping(value = "/{id}")
+  @ResponseBody
   public ResponseEntity<Account> getAccountById(@PathVariable("id") Long id) {
     Account account = accountService.getAccountById(id);
     return new ResponseEntity<>(account, HttpStatus.OK);
