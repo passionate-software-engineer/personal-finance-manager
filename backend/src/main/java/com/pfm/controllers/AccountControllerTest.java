6fcd1fc00410ebc193c0056952fe567bfa7d76f2
piotrhawlicki <piotrek.hawlicki@gmail.com>
2018-06-25 21:12:14 +0200

    next test

--- backend/src/main/java/com/pfm/controllers/AccountControllerTest.java
+++ backend/src/main/java/com/pfm/controllers/AccountControllerTest.java
@@ -1,7 +1,10 @@
 package com.pfm.controllers;
 
+import org.junit.Before;
+import org.junit.FixMethodOrder;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -13,6 +16,7 @@ import org.springframework.test.web.servlet.ResultMatcher;
 import static org.springframework.mock.http.server.reactive.MockServerHttpRequest.get;
 import static org.springframework.test.web.client.match.MockRestRequestMatchers.content;
 import static org.springframework.test.web.client.match.MockRestRequestMatchers.jsonPath;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.hamcrest.Matchers.is;
@@ -25,6 +29,18 @@ public class AccountControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
+
+
+    @org.junit.Test
+    public void shouldAddAccountTest() throws Exception {
+
+        String accountJson = "{\"id\":1,\"name\":\"Piotrek\",\"balance\":\"100\"}";
+
+        this.mockMvc.perform(post("/accounts/")
+                .contentType("application/json;charset=UTF-8")
+                .content(accountJson))
+                .andExpect(status().isCreated());
+    }
 
     @Test
     public void getAccountById() throws Exception {
@@ -40,10 +56,6 @@ public class AccountControllerTest {
     public void getAllAccounts() {
     }
 
-    @org.junit.Test
-    public void shouldAddAccountTest() {
-
-    }
 
     @org.junit.Test
     public void updateAccount() {
