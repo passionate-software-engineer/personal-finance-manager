53d6a99c68dd14e032e7e1d14f2a2cca1306fba3
piotrhawlicki <piotrek.hawlicki@gmail.com>
2018-06-26 22:20:49 +0200

    Change import for test. 4 good test

--- backend/src/main/java/com/pfm/controllers/AccountControllerTest.java
+++ backend/src/main/java/com/pfm/controllers/AccountControllerTest.java
@@ -1,6 +1,5 @@
 package com.pfm.controllers;
 
-
 import org.junit.FixMethodOrder;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -8,22 +7,25 @@ import org.junit.runners.MethodSorters;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.RequestBuilder;
-import org.springframework.test.web.servlet.ResultMatcher;
+
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-import static org.springframework.mock.http.server.reactive.MockServerHttpRequest.get;
-import static org.springframework.test.web.client.match.MockRestRequestMatchers.content;
-import static org.springframework.test.web.client.match.MockRestRequestMatchers.jsonPath;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -51,10 +53,10 @@ public class AccountControllerTest {
     public void Test2getAccountById() throws Exception {
 
         this.mockMvc
-                .perform((RequestBuilder) get("/accounts/1"))
-                .andExpect((ResultMatcher) content().contentType("application/json;charset=UTF-8"))
+                .perform(get("/accounts/1"))
+                .andExpect(content().contentType("application/json;charset=UTF-8"))
                 .andDo(print()).andExpect(status().isOk())
-                .andExpect((ResultMatcher) jsonPath("$.id", is(1)));
+                .andExpect(jsonPath("$.id", is(1)));
     }
 
     @org.junit.Test
@@ -63,7 +65,7 @@ public class AccountControllerTest {
 
 
     @org.junit.Test
-    public void updateAccount() throws Exception {
+    public void Test3updateAccount() throws Exception {
 
         String accountJson = "{\"id\":1,\"name\":\"Piotrek\",\"balance\":\"100\"}";
 
@@ -85,23 +87,23 @@ public class AccountControllerTest {
 
                 .andExpect(MockMvcResultMatchers.jsonPath("$.id", is(1)))
                 .andExpect(MockMvcResultMatchers.jsonPath("$.name", is("Jacek")))
-                .andExpect(MockMvcResultMatchers.jsonPath("$.balance", is("200")));
+                .andExpect(MockMvcResultMatchers.jsonPath("$.balance", is(200.00)));
 
     }
 
 
-    @org.junit.Test
-    public void Test3deleteAccount() throws Exception {
+    @Test
+    public void Test4deleteAccount() throws Exception {
 
         String accountJson = "{\"id\":1,\"name\":\"Piotrek\",\"balance\":\"100\"}";
         this.mockMvc.perform(post("/accounts/")
-                .contentType("application/json;charset=UTF-8")
+                .contentType(MediaType.valueOf("application/json;charset=UTF-8"))
                 .content(accountJson))
                 .andExpect(status().isCreated());
 
         this.mockMvc
                 .perform(delete("/accounts/1"))
-                .andDo(print()).andExpect(status().isOk())
-                .andExpect(status().isOk());
+//                .andDo(print()).andExpect(status().isOk())
+                .andExpect(status().isNoContent());
     }
 }
