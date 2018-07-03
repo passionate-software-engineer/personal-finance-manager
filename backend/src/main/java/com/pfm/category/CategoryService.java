4d051c199dd5593527cedf78d53b5d5f3b4a9587
Przemyslaw Hawlicki <32299547+phawlicki@users.noreply.github.com>
2018-07-03 21:10:37 +0200

    Added Category Service

---
+++ backend/src/main/java/com/pfm/category/CategoryService.java
@@ -0,0 +1,46 @@
+package com.pfm.category;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+@Slf4j
+@Service
+@AllArgsConstructor
+public class CategoryService {
+
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    public Category getCategoryById(long id) {
+        return categoryRepository.findById(id).get();
+    }
+
+    public List<Category> getCategories() {
+      List<Category> categories = new ArrayList<>();
+      categoryRepository.findAll().forEach(category -> categories.add(category));
+      categories.sort(Comparator.comparing(Category::getId));
+        return categories;
+    }
+
+    public Category addCategory(Category category) {
+        return categoryRepository.save(category);
+    }
+
+    public void removeCategory(long id) {
+        categoryRepository.deleteById(id);
+    }
+
+    public Category updateCategory(long id, String name, Category parentCategory) {
+        Category categoryToUpdate = getCategoryById(id);
+        categoryToUpdate.setName(name);
+        categoryToUpdate.setParentCategory(parentCategory);
+        categoryRepository.save(categoryToUpdate);
+        return categoryToUpdate;
+    }
+}
