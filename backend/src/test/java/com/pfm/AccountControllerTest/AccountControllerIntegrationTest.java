2fa49f102613c98c1e4834c652b297dcee69bc74
piotrhawlicki <piotrek.hawlicki@gmail.com>
2018-07-04 19:27:11 +0200

    change format

--- backend/src/test/java/com/pfm/AccountControllerTest/AccountControllerIntegrationTest.java
+++ backend/src/test/java/com/pfm/AccountControllerTest/AccountControllerIntegrationTest.java
@@ -35,8 +35,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest
 @AutoConfigureMockMvc
 @DirtiesContext (classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
-
-public class AccountControllerIntegrationTest{
+public class AccountControllerIntegrationTest {
 
     public List<String> readFile() throws IOException {
         FileReader fileReader = new FileReader("src\\test\\resources\\account.txt");
