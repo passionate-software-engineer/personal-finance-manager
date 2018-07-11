1fff4b48f98b4410d10b58a09fe45db0c1a11450
piotrhawlicki <piotrek.hawlicki@gmail.com>
2018-07-11 16:10:21 +0200

    swagger basic configuration

---
+++ backend/src/main/java/com/pfm/config/SwaggerConfig.java
@@ -0,0 +1,9 @@
+package com.pfm.config;
+
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@EnableSwagger2
+@Configuration
+public class SwaggerConfig {
+}
