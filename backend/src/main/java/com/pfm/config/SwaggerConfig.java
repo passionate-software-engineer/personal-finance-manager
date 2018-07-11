5edb7c8b503b5c5a15f619e469a3221870100f71
piotrhawlicki <piotrek.hawlicki@gmail.com>
2018-07-11 16:51:11 +0200

    swagger nest config

--- backend/src/main/java/com/pfm/config/SwaggerConfig.java
+++ backend/src/main/java/com/pfm/config/SwaggerConfig.java
@@ -1,9 +1,18 @@
 package com.pfm.config;
 
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-@EnableSwagger2
+@Setter
 @Configuration
+@EnableSwagger2
+@ConfigurationProperties(prefix = "swagger.swagger")
 public class SwaggerConfig {
+
+    private String title;
+    private String description;
+    private String path;
+
 }
