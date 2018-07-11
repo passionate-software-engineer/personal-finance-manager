8aed4bc5a4f64992bb72ade5fa33c69c9f2948eb
piotrhawlicki <piotrek.hawlicki@gmail.com>
2018-07-11 17:10:38 +0200

    reformat code

--- backend/src/main/java/com/pfm/config/SwaggerConfig.java
+++ backend/src/main/java/com/pfm/config/SwaggerConfig.java
@@ -12,9 +12,7 @@ import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
 import java.util.Collections;
-
 import static com.google.common.base.Predicates.and;
 import static springfox.documentation.builders.PathSelectors.regex;
 
