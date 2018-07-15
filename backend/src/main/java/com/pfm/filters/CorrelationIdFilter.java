dfc5f7031869dec6f119585912cf483418e2ed8c
Piotr Kołacz <pio-kol@users.noreply.github.com>
2018-07-15 18:46:55 +0000

    Corrected logging and added Correlation id handling (#72)

---
+++ backend/src/main/java/com/pfm/filters/CorrelationIdFilter.java
@@ -0,0 +1,43 @@
+package com.pfm.filters;
+
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.MDC;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.UUID;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Slf4j
+@Component
+public class CorrelationIdFilter extends OncePerRequestFilter {
+
+  @Override
+  protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
+      FilterChain filterChain) throws IOException, ServletException {
+    try {
+      addRequestCorrelationIdToMDC(request);
+      filterChain.doFilter(request, response);
+    } finally {
+      removeRequestCorrelationIdFromMDC();
+    }
+  }
+
+  private void addRequestCorrelationIdToMDC(HttpServletRequest request) {
+    String correlationId = request.getHeader("correlationId");
+
+    if (correlationId == null) {
+      correlationId = UUID.randomUUID().toString();
+    }
+
+    MDC.put("correlationId", correlationId);
+  }
+
+  private void removeRequestCorrelationIdFromMDC() {
+    MDC.remove("correlationId");
+  }
+}
