39a0d59de134f07b998f726dc6848e69eb0fd935
Bartek S <kursantjava68@gmail.com>
2018-07-29 11:25:55 +0200

    Logging to category controller was added.

--- backend/src/main/java/com/pfm/category/CategoryController.java
+++ backend/src/main/java/com/pfm/category/CategoryController.java
@@ -38,20 +38,26 @@ public class CategoryController {
   private CategoryService categoryService;
   private CategoryValidator categoryValidator;
 
-  // TODO - add loging similar way it is added in Account Controller
   @ApiOperation(value = "Find category by id", response = Category.class)
   @GetMapping(value = "/{id}")
   public ResponseEntity<Category> getCategoryById(@PathVariable long id) {
+
+    log.info("Retrieving category with id: {}", id);
     Optional<Category> category = categoryService.getCategoryById(id);
+
     if (!category.isPresent()) {
+      log.info("Category with id {} was not found", id);
       return ResponseEntity.notFound().build();
     }
+    log.info("Category with id {} was successfully retrieved", id);
     return ResponseEntity.ok(category.get());
   }
 
   @ApiOperation(value = "Get list of categories", response = Category.class, responseContainer = "List")
   @GetMapping
   public ResponseEntity<List<Category>> getCategories() {
+
+    log.info("Retrieving all category from database");
     List<Category> categories = categoryService.getCategories();
     return ResponseEntity.ok(categories);
   }
@@ -59,13 +65,17 @@ public class CategoryController {
   @ApiOperation(value = "Create a new category", response = Long.class)
   @PostMapping
   public ResponseEntity<?> addCategory(@RequestBody CategoryRequest categoryRequest) {
+
+    log.info("Saving category {} to the database", categoryRequest.getName());
     Category category = convertToCategory(categoryRequest);
 
     List<String> validationResult = categoryValidator.validateCategoryForAdd(category);
     if (!validationResult.isEmpty()) {
+      log.info("Category is not valid {}", validationResult);
       return ResponseEntity.badRequest().body(validationResult);
     }
     Category createdCategory = categoryService.addCategory(category);
+    log.info("Saving category to the database was successful. Category id is {}", createdCategory.getId());
     return ResponseEntity.ok(createdCategory.getId());
   }
 
@@ -73,7 +83,9 @@ public class CategoryController {
   @PutMapping(value = "/{id}")
   public ResponseEntity<?> updateCategory(@PathVariable long id,
       @RequestBody CategoryRequest categoryRequest) {
+
     if (!categoryService.idExist(id)) {
+      log.info("No category with id {} was found, not able to update", id);
       return ResponseEntity.notFound().build();
     }
 
@@ -82,23 +94,31 @@ public class CategoryController {
 
     List<String> validationResult = categoryValidator.validateCategoryForUpdate(category);
     if (!validationResult.isEmpty()) {
+      log.error("Category is not valid {}", validationResult);
       return ResponseEntity.badRequest().body(validationResult);
     }
 
     categoryService.updateCategory(id, category);
+    log.info("Category with id {} was successfully updated", id);
     return ResponseEntity.ok().build();
   }
 
   @ApiOperation(value = "Delete an existing category", response = Void.class)
   @DeleteMapping(value = "/{id}")
   public ResponseEntity<?> deleteCategory(@PathVariable long id) {
+
     if (!categoryService.getCategoryById(id).isPresent()) {
+      log.info("No category with id {} was found, not able to delete", id);
       return ResponseEntity.notFound().build();
     }
     if (categoryService.isParentCategory(id)) {
+      log.info("Category is parent category. Delete category {} not possible", id);
       return ResponseEntity.badRequest().body(getMessage(CANNOT_DELETE_PARENT_CATEGORY));
     }
+    log.info("Attempting to delete category with id {}", id);
     categoryService.deleteCategory(id);
+
+    log.info("Category with id {} was deleted successfully", id);
     return ResponseEntity.ok().build();
   }
 
