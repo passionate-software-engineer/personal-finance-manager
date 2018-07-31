6ce1b226c56b43a63bc8cf9197d07cfd95a22cbf
Przemyslaw <phawlicki@gmail.com>
2018-07-31 16:47:34 +0200

    Transaction controller tests

--- backend/src/test/java/com/pfm/transaction/TransactionControllerIntegrationTest.java
+++ backend/src/test/java/com/pfm/transaction/TransactionControllerIntegrationTest.java
@@ -1,14 +1,16 @@
 package com.pfm.transaction;
 
-import static com.pfm.helpers.TestAccountProvider.ACCOUNT_JACEK_BALANCE_1000;
 import static com.pfm.helpers.TestTransactionProvider.MOCK_TRANSACTION_ACCOUNT;
 import static com.pfm.helpers.TestTransactionProvider.MOCK_TRANSACTION_CATEGORY;
-import static com.pfm.helpers.TestTransactionProvider.mockTransaction;
+
 import static com.pfm.helpers.TestTransactionProvider.transactionRequest;
+import static com.pfm.helpers.TestTransactionProvider.transactionRequest1;
+import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.is;
-import static org.junit.Assert.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -18,7 +20,9 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.pfm.account.Account;
 import com.pfm.category.Category;
 import com.pfm.transaction.TransactionController.TransactionRequest;
+
 import org.flywaydb.core.Flyway;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -59,7 +63,6 @@ public class TransactionControllerIntegrationTest {
 
   @Test
   public void shouldAddTransaction() throws Exception {
-//given
     callRestServiceToAddAccount(MOCK_TRANSACTION_ACCOUNT);
     callRestServiceToAddCategory(MOCK_TRANSACTION_CATEGORY);
 
@@ -82,17 +85,67 @@ public class TransactionControllerIntegrationTest {
   }
 
   @Test
-  public void getTransactions() {
+  public void getTransactions() throws Exception {
+    callRestServiceToAddAccount(MOCK_TRANSACTION_ACCOUNT);
+    callRestServiceToAddCategory(MOCK_TRANSACTION_CATEGORY);
+    callRestServiceToAddTransaction(transactionRequest);
+    callRestServiceToAddTransaction(transactionRequest1);
+
+    this.mockMvc
+        .perform(get(TRANSACTION_SERVICE_PATH))
+        .andExpect(content().contentType(CONTENT_TYPE))
+        .andDo(print()).andExpect(status().isOk()).andExpect(jsonPath("$.[0].id", is(1)))
+        .andExpect(jsonPath("$.[0].description", is("Cinema")))
+        .andExpect(jsonPath("$.[0].category.id", is(1)))
+        .andExpect(jsonPath("$.[0].category.name", is("entertainment")))
+        .andExpect(jsonPath("$.[0].account.id", is(1)))
+        .andExpect(jsonPath("$.[0].account.name", is("ING BANK")))
+        .andExpect(jsonPath("$.[0].account.balance", is(equalTo("1500.00"))))
+        .andExpect(jsonPath("$.[0].price", is(equalTo("150.00"))))
+        .andExpect(jsonPath("$.[1].id", is(2)))
+        .andExpect(jsonPath("$.[1].description", is("Food")))
+        .andExpect(jsonPath("$.[1].category.id", is(1)))
+        .andExpect(jsonPath("$.[1].category.name", is("entertainment")))
+        .andExpect(jsonPath("$.[1].account.id", is(1)))
+        .andExpect(jsonPath("$.[1].account.name", is("ING BANK")))
+        .andExpect(jsonPath("$.[1].account.balance", is(equalTo("1500.00"))))
+        .andExpect(jsonPath("$.[1].price", is(equalTo("600.00"))));
   }
 
-
   @Test
-  public void updateAccount() {
+  public void updateAccount() throws Exception {
+    callRestServiceToAddAccount(MOCK_TRANSACTION_ACCOUNT);
+    callRestServiceToAddCategory(MOCK_TRANSACTION_CATEGORY);
+    callRestServiceToAddTransaction(transactionRequest);
+
+    this.mockMvc.perform(put(TRANSACTION_SERVICE_PATH + "/1")
+        .contentType(CONTENT_TYPE)
+        .content(jsonTransaction(transactionRequest1)))
+        .andDo(print())
+        .andExpect(status().isOk());
+    this.mockMvc
+        .perform(get(TRANSACTION_SERVICE_PATH))
+        .andExpect(content().contentType(CONTENT_TYPE))
+        .andDo(print()).andExpect(status().isOk())
+        .andExpect(jsonPath("$.[0].id", is(1)))
+        .andExpect(jsonPath("$.[0].description", is("Food")))
+        .andExpect(jsonPath("$.[0].category.id", is(1)))
+        .andExpect(jsonPath("$.[0].category.name", is("entertainment")))
+        .andExpect(jsonPath("$.[0].account.id", is(1)))
+        .andExpect(jsonPath("$.[0].account.name", is("ING BANK")))
+        .andExpect(jsonPath("$.[0].account.balance", is(equalTo("1500.00"))))
+        .andExpect(jsonPath("$.[0].price", is(equalTo("600.00"))));
   }
 
   @Test
-  public void deleteTransaction() {
+  public void deleteTransaction() throws Exception {
+    callRestServiceToAddAccount(MOCK_TRANSACTION_ACCOUNT);
+    callRestServiceToAddCategory(MOCK_TRANSACTION_CATEGORY);
+    callRestServiceToAddTransaction(transactionRequest);
 
+    this.mockMvc
+        .perform(delete(TRANSACTION_SERVICE_PATH + "/1"))
+        .andExpect(status().isOk());
   }
 
   private void callRestServiceToAddAccount(Account account) throws Exception {
@@ -128,6 +181,4 @@ public class TransactionControllerIntegrationTest {
   private String jsonCategory(Category category) throws Exception {
     return mapper.writeValueAsString(category);
   }
-
-
 }
