22dfb6ed98ee7747e4439811e6c8a8626362027f
Sebastian Pacuk <spacuk@atlassian.com>
2018-08-11 22:34:33 +0200

    simplied logic for updating account amount after adding, updating, deleting transactions

--- backend/src/main/java/com/pfm/transaction/TransactionService.java
+++ backend/src/main/java/com/pfm/transaction/TransactionService.java
@@ -2,6 +2,7 @@ package com.pfm.transaction;
 
 import com.pfm.account.Account;
 import com.pfm.account.AccountService;
+import java.math.BigDecimal;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
@@ -30,15 +31,8 @@ public class TransactionService {
   }
 
   public Transaction addTransaction(Transaction transaction) {
-    Optional<Account> transactionAccount = accountService
-        .getAccountById(transaction.getAccount().getId());
-    if (!transactionAccount.isPresent()) {
-      throw new IllegalStateException(
-          "Account with id: " + transaction.getAccount().getId() + " not exist in DB");
-    }
-    Account accountToUpdate = transactionAccount.get();
-    accountToUpdate.setBalance(accountToUpdate.getBalance().subtract(transaction.getPrice()));
-    accountService.updateAccount(accountToUpdate.getId(), accountToUpdate);
+    substractAmountToAccountWhileProcessingTransaction(transaction.getAccount().getId(),
+        transaction.getPrice());
     return transactionRepository.save(transaction);
   }
 
@@ -52,68 +46,66 @@ public class TransactionService {
 
     Transaction transactionToUpdate = transactionFromDb.get();
 
-    Optional<Account> transactionAccountBeforeUpdate = accountService
-        .getAccountById(transactionToUpdate.getAccount().getId());
-
-    if (!transactionAccountBeforeUpdate.isPresent()) {
-      throw new IllegalStateException(
-          "Account with id: " + transactionToUpdate.getAccount().getId() + " not exist in DB");
-    }
-
-    Account accountToUpdate = transactionAccountBeforeUpdate.get();
-    accountToUpdate
-        .setBalance(accountToUpdate.getBalance().add(transactionToUpdate.getPrice()));
-    accountService.updateAccount(accountToUpdate.getId(), accountToUpdate);
-
+    addAmountToAccountWhileProcessingTransaction(transactionToUpdate.getAccount().getId(),
+        transactionToUpdate.getPrice());
+      
     transactionToUpdate.setDescription(transaction.getDescription());
     transactionToUpdate.setCategory(transaction.getCategory());
     transactionToUpdate.setPrice(transaction.getPrice());
     transactionToUpdate.setAccount(transaction.getAccount());
     transactionToUpdate.setDate(transaction.getDate());
 
-    Optional<Account> transactionAccountAfterUpdate = accountService
-        .getAccountById(transactionToUpdate.getAccount().getId());
-
-    if (!transactionAccountAfterUpdate.isPresent()) {
-      throw new IllegalStateException(
-          "Account with id: " + transactionToUpdate.getAccount().getId() + " not exist in DB");
-    }
-
-    Account accountToUpdateAfterTransactionUpdate = transactionAccountAfterUpdate.get();
-    accountToUpdateAfterTransactionUpdate
-        .setBalance(
-            accountToUpdateAfterTransactionUpdate.getBalance().subtract(transaction.getPrice()));
-
-    accountService.updateAccount(accountToUpdateAfterTransactionUpdate.getId(),
-        accountToUpdateAfterTransactionUpdate);
+    substractAmountToAccountWhileProcessingTransaction(transactionToUpdate.getAccount().getId(),
+        transactionToUpdate.getPrice());
 
     transactionRepository.save(transactionToUpdate);
   }
 
   public void deleteTransaction(long id) {
-    Optional<Transaction> transactionToDelete = getTransactionById(id);
+    Optional<Transaction> transactionFromDb = getTransactionById(id);
 
-    if (!transactionToDelete.isPresent()) {
+    if (!transactionFromDb.isPresent()) {
       throw new IllegalStateException("Transaction with id: " + id + " not exist in DB");
     }
 
-    long transactionAccountId = transactionToDelete.get().getAccount().getId();
-    Optional<Account> transactionAccount = accountService
-        .getAccountById(transactionAccountId);
-
-    if (!transactionAccount.isPresent()) {
-      throw new IllegalStateException(
-          "Account with id: " + transactionAccountId + " not exist in DB");
-    }
-    Account accountToUpdate = transactionAccount.get();
-    accountToUpdate
-        .setBalance(accountToUpdate.getBalance().add(transactionToDelete.get().getPrice()));
-    accountService.updateAccount(accountToUpdate.getId(), accountToUpdate);
+    Transaction transactionToDelete = transactionFromDb.get();
+    addAmountToAccountWhileProcessingTransaction(transactionToDelete.getAccount().getId(),
+        transactionToDelete.getPrice());
     transactionRepository.deleteById(id);
   }
 
   public boolean idExist(long id) {
     return transactionRepository.existsById(id);
+  }
+
+  private void addAmountToAccountWhileProcessingTransaction(long accountId,
+      BigDecimal amountToAdd) {
+    Optional<Account> transactionAccount = accountService
+        .getAccountById(accountId);
+
+    if (!transactionAccount.isPresent()) {
+      throw new IllegalStateException(
+          "Account with id: " + accountId + " not exist in DB");
+    }
+    Account accountToUpdate = transactionAccount.get();
+    accountToUpdate
+        .setBalance(accountToUpdate.getBalance().add(amountToAdd));
+    accountService.updateAccount(accountToUpdate.getId(), accountToUpdate);
+  }
+
+  private void substractAmountToAccountWhileProcessingTransaction(long accountId,
+      BigDecimal amountToAdd) {
+    Optional<Account> transactionAccount = accountService
+        .getAccountById(accountId);
+
+    if (!transactionAccount.isPresent()) {
+      throw new IllegalStateException(
+          "Account with id: " + accountId + " not exist in DB");
+    }
+    Account accountToUpdate = transactionAccount.get();
+    accountToUpdate
+        .setBalance(accountToUpdate.getBalance().subtract(amountToAdd));
+    accountService.updateAccount(accountToUpdate.getId(), accountToUpdate);
   }
 
 }
