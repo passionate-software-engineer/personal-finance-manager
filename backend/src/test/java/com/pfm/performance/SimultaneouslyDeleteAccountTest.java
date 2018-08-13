50fe7aa7847b7f8155770377d3b6007b27f06b4b
Karol Kowalkowskil <kkowalkowski@op.pl>
2018-08-13 23:35:00 +0200

    Multithreading tests

--- backend/src/test/java/com/pfm/performance/SimultaneouslyDeleteAccountTest.java
+++ backend/src/test/java/com/pfm/performance/SimultaneouslyDeleteAccountTest.java
@@ -1,22 +1,10 @@
 package com.pfm.performance;
 
-import static com.jayway.restassured.RestAssured.given;
-import static org.hamcrest.Matchers.is;
-import static org.junit.Assert.assertThat;
-
 import com.anarsoft.vmlens.concurrent.junit.ConcurrentTestRunner;
 import com.anarsoft.vmlens.concurrent.junit.ThreadCount;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.pfm.account.Account;
-
-import java.math.BigDecimal;
-import java.math.RoundingMode;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.UUID;
-
 import org.junit.After;
 import org.junit.ClassRule;
 import org.junit.Rule;
@@ -28,14 +16,23 @@ import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.test.context.junit4.rules.SpringClassRule;
 import org.springframework.test.context.junit4.rules.SpringMethodRule;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.List;
+import java.util.UUID;
+
+import static com.jayway.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
 
 @RunWith(ConcurrentTestRunner.class)
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
-public class SimultaneouslyAddAccountTest {
-    private static final String INVOICES_SERVICE_PATH = "http://localhost:%d/accounts";
-    private final static int THREAD_COUNT = 24; //set how much threads you want to start
+public class SimultaneouslyDeleteAccountTest {
 
-    private List<Account> addedAccounts = Collections.synchronizedList(new ArrayList<>());
+
+    private static final String INVOICES_SERVICE_PATH = "http://localhost:%d/accounts";
+    private static final int THREAD_COUNT = 24; //set how much threads you want to start
     private ObjectMapper objectMapper = new ObjectMapper();
 
     @ClassRule
@@ -53,28 +50,20 @@ public class SimultaneouslyAddAccountTest {
 
     @Test
     @ThreadCount(THREAD_COUNT)
-    public void shouldAddSimultaneouslyAccountTest() throws Exception {
+    public void shouldDeleteSimultaneouslyAccountTest() throws Exception {
         Account tempAccount = Account.builder()
                 .name(UUID.randomUUID().toString())
                 .balance(BigDecimal.valueOf((long) (Math.random() * Integer.MAX_VALUE)).setScale(2, RoundingMode.CEILING))
                 .build();
 
         tempAccount.setId(Long.parseLong(given().contentType("application/json").body(tempAccount).when().post(servicePath()).getBody().asString()));
-        addedAccounts.add(tempAccount);
+        assertThat(given().when().delete(servicePath() + "/" + tempAccount.getId().toString()).statusCode(), is(200));
     }
 
     @After
     public void afterCheck() throws Exception {
-        addedAccounts.sort((first, second) -> (int) (first.getId() - second.getId()));
-
         List<Account> accounts = getAccounts();
-        assertThat(accounts.size(), is(THREAD_COUNT));
-
-        int index = 0;
-        for (Account account : accounts) {
-            assertThat(account, is(addedAccounts.get(index++))); // TODO use error collector
-            assertThat(given().when().delete(servicePath() + "/" + account.getId().toString()).statusCode(), is(200));
-        }
+        assertThat(accounts.size(), is(0));
     }
 
     private List<Account> getAccounts() throws Exception {
--- backend/src/test/java/com/pfm/performance/SimultaneouslyUpdateAccountTest.java
+++ backend/src/test/java/com/pfm/performance/SimultaneouslyUpdateAccountTest.java
@@ -1,22 +1,10 @@
 package com.pfm.performance;
 
-import static com.jayway.restassured.RestAssured.given;
-import static org.hamcrest.Matchers.is;
-import static org.junit.Assert.assertThat;
-
 import com.anarsoft.vmlens.concurrent.junit.ConcurrentTestRunner;
 import com.anarsoft.vmlens.concurrent.junit.ThreadCount;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.pfm.account.Account;
-
-import java.math.BigDecimal;
-import java.math.RoundingMode;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.UUID;
-
 import org.junit.After;
 import org.junit.ClassRule;
 import org.junit.Rule;
@@ -28,10 +16,22 @@ import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.test.context.junit4.rules.SpringClassRule;
 import org.springframework.test.context.junit4.rules.SpringMethodRule;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.UUID;
+
+import static com.jayway.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
 
 @RunWith(ConcurrentTestRunner.class)
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
-public class SimultaneouslyAddAccountTest {
+public class SimultaneouslyUpdateAccountTest {
+
     private static final String INVOICES_SERVICE_PATH = "http://localhost:%d/accounts";
     private final static int THREAD_COUNT = 24; //set how much threads you want to start
 
@@ -46,22 +46,25 @@ public class SimultaneouslyAddAccountTest {
 
     @LocalServerPort
     private int port;
-
     private String servicePath() {
         return String.format(INVOICES_SERVICE_PATH, port);
     }
 
+
     @Test
     @ThreadCount(THREAD_COUNT)
-    public void shouldAddSimultaneouslyAccountTest() throws Exception {
+    public void shouldUpdateSimultaneouslyAccountTest() throws Exception {
         Account tempAccount = Account.builder()
                 .name(UUID.randomUUID().toString())
-                .balance(BigDecimal.valueOf((long) (Math.random() * Integer.MAX_VALUE)).setScale(2, RoundingMode.CEILING))
+                .balance(BigDecimal.valueOf(-1d))
                 .build();
 
         tempAccount.setId(Long.parseLong(given().contentType("application/json").body(tempAccount).when().post(servicePath()).getBody().asString()));
+        tempAccount.setBalance(BigDecimal.valueOf((long) (Math.random() * Integer.MAX_VALUE)).setScale(2, RoundingMode.CEILING));
         addedAccounts.add(tempAccount);
+        assertThat(given().contentType("application/json").body(tempAccount).when().put(servicePath()+"/"+tempAccount.getId().toString()).statusCode(),is(200));
     }
+
 
     @After
     public void afterCheck() throws Exception {
@@ -76,6 +79,7 @@ public class SimultaneouslyAddAccountTest {
             assertThat(given().when().delete(servicePath() + "/" + account.getId().toString()).statusCode(), is(200));
         }
     }
+
 
     private List<Account> getAccounts() throws Exception {
         return objectMapper.readValue(given().when().get(servicePath()).getBody().asString(), new TypeReference<List<Account>>() {
