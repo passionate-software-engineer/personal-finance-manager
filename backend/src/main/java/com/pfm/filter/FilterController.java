80abd4e86512f8834f3fb54ef5f86d09352736f1
Sebastian Pacuk <spacuk@atlassian.com>
2018-08-15 16:36:04 +0200

    Start

---
+++ backend/src/main/java/com/pfm/filter/FilterController.java
@@ -0,0 +1,12 @@
+package com.pfm.filter;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@AllArgsConstructor
+@RestController
+public class FilterController {
+
+}
---
+++ backend/src/main/java/com/pfm/filter/FilterRequest.java
@@ -0,0 +1,36 @@
+package com.pfm.filter;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import io.swagger.annotations.ApiModelProperty;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.List;
+import javax.persistence.OneToMany;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class FilterRequest {
+
+  private String name;
+
+  private List<Long> accountsIds;
+
+  private List<Long> categoryIds;
+
+  private BigDecimal priceFrom;
+  private BigDecimal priceTo;
+
+  private LocalDate dateFrom;
+  private LocalDate dateTo;
+
+  private String description;
+}
