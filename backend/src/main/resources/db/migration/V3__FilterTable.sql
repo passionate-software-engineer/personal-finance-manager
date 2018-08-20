f8b0bdc63983079da4e9c3da35239f67d647c919
Sebastian Pacuk <spacuk@atlassian.com>
2018-08-20 23:07:23 +0200

    finally managed to save filter in DB , this i just test version

--- backend/src/main/resources/db/migration/V3__FilterTable.sql
+++ backend/src/main/resources/db/migration/V3__FilterTable.sql
@@ -4,18 +4,20 @@ CREATE TABLE filter
   name         VARCHAR(255) NOT NULL,
   description  VARCHAR(255) NOT NULL,
   price_from    NUMERIC(19, 2),
-  price_to      NUMERIC(19, 2)
+  price_to      NUMERIC(19, 2),
+  date_from     DATE,
+  date_to       DATE
 );
 
-CREATE TABLE filter_accounts
+CREATE TABLE filter_accounts_ids
 (
   filter_id           BIGINT,
-  accounts_id         BIGINT
+  accounts_ids         BIGINT
 );
 
-CREATE TABLE filter_categories
+CREATE TABLE filter_categories_ids
 (
   filter_id           BIGINT,
-  categories_id         BIGINT
+  categories_ids       BIGINT
 );
 
