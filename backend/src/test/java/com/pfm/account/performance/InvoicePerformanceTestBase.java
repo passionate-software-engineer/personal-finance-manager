d9feddea79191838acac5fb8f622800b17d4ddf0
Karol K <33834519+karolkowalkowski@users.noreply.github.com>
2018-08-23 22:19:22 +0200

    Multithread2 (#106)
    * Multithreading - Proof of Concept
    
    * Little correction
    
    * Multithreading - Proof of Concept
    
    * Multithread KK
    
    * Multithreading tests
    
    * Multithread tests after review
    
    * Multithread tests with base class
    
    * Multithread - fixed style
    
    * Refactored Performance tests.
    
    * Refactored Performance tests.

---
+++ backend/src/test/java/com/pfm/account/performance/InvoicePerformanceTestBase.java
@@ -0,0 +1,118 @@
+package com.pfm.account.performance;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+import com.anarsoft.vmlens.concurrent.junit.ConcurrentTestRunner;
+import com.pfm.account.Account;
+import io.restassured.http.ContentType;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.UUID;
+import javax.annotation.PostConstruct;
+import org.junit.After;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.rules.ErrorCollector;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.test.context.junit4.rules.SpringClassRule;
+import org.springframework.test.context.junit4.rules.SpringMethodRule;
+
+@RunWith(ConcurrentTestRunner.class)
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+public abstract class InvoicePerformanceTestBase {
+
+  private static final String INVOICES_SERVICE_PATH = "http://localhost:%d/accounts";
+
+  protected static final int THREAD_COUNT = 24;
+
+  @LocalServerPort
+  private int port;
+
+  @ClassRule
+  public static final SpringClassRule SPRING_CLASS_RULE = new SpringClassRule();
+
+  @Rule
+  public final SpringMethodRule springMethodRule = new SpringMethodRule();
+
+  @Rule
+  public final ErrorCollector collector = new ErrorCollector();
+
+  protected List<Account> accounts = Collections.synchronizedList(new ArrayList<>());
+
+  protected Account[] getAccounts() throws Exception {
+    return given()
+        .when()
+        .get(invoiceServicePath())
+        .getBody()
+        .as(Account[].class);
+  }
+
+  protected BigDecimal getRandomBalance() {
+    return BigDecimal.valueOf((long) (Math.random() * Integer.MAX_VALUE)).setScale(2, RoundingMode.CEILING);
+  }
+
+  protected String getRandomName() {
+    return UUID.randomUUID().toString();
+  }
+
+  protected String invoiceServicePath() {
+    return String.format(INVOICES_SERVICE_PATH, port);
+  }
+
+  protected String invoiceServicePath(long id) {
+    return invoiceServicePath() + "/" + id;
+  }
+
+  @PostConstruct
+  public void before() {
+    for (int i = 0; i < 10; ++i) {
+      Account account = Account.builder()
+          .name(getRandomName())
+          .balance(getRandomBalance())
+          .build();
+
+      String response = given()
+          .contentType(ContentType.JSON)
+          .body(account)
+          .when()
+          .post(invoiceServicePath())
+          .getBody()
+          .asString();
+
+      Long accountId = Long.parseLong(response);
+      account.setId(accountId);
+
+      accounts.add(account);
+    }
+  }
+
+  @After
+  public void afterCheck() throws Exception {
+    accounts.sort((first, second) -> (int) (first.getId() - second.getId()));
+
+    Account[] accountsFromService = getAccounts();
+    assertThat(accountsFromService.length, is(accounts.size()));
+
+    int index = 0;
+    for (Account account : accountsFromService) {
+      collector.checkThat(account, is(equalTo(accounts.get(index++))));
+    }
+
+    for (Account account : accountsFromService) {
+      given()
+          .when()
+          .delete(invoiceServicePath(account.getId()));
+    }
+
+    assertThat(getAccounts().length, is(0));
+  }
+}
---
+++ backend/src/test/java/com/pfm/account/performance/UpdateAccountTest.java
@@ -0,0 +1,34 @@
+package com.pfm.account.performance;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.is;
+
+import com.anarsoft.vmlens.concurrent.junit.ThreadCount;
+import com.pfm.account.Account;
+import io.restassured.http.ContentType;
+import java.util.concurrent.atomic.AtomicInteger;
+import org.junit.Test;
+
+public class UpdateAccountTest extends InvoicePerformanceTestBase {
+
+  private AtomicInteger counter = new AtomicInteger(0);
+
+  @Test
+  @ThreadCount(THREAD_COUNT)
+  public void shouldUpdateSimultaneouslyMultipleAccounts() {
+
+    Account account = accounts.get(counter.getAndIncrement());
+    account.setBalance(getRandomBalance());
+    account.setName(getRandomName());
+
+    int statusCode = given()
+        .contentType(ContentType.JSON)
+        .body(account)
+        .when()
+        .put(invoiceServicePath(account.getId()))
+        .statusCode();
+
+    collector.checkThat(statusCode, is(200));
+  }
+
+}
