561b6222bb8443ea863745645f1c152c384d7e6e
Sebastian Pacuk <spacuk@atlassian.com>
2018-08-25 23:14:38 +0200

    code coverage = 100%

--- backend/src/main/java/com/pfm/filter/Filter.java
+++ backend/src/main/java/com/pfm/filter/Filter.java
@@ -45,4 +45,5 @@ public final class Filter {
   private LocalDate dateTo;
 
   private String description;
+
 }
--- backend/src/main/java/com/pfm/filter/FilterRequest.java
+++ backend/src/main/java/com/pfm/filter/FilterRequest.java
@@ -14,6 +14,7 @@ import lombok.Setter;
 @Getter
 @Setter
 @Builder
+@NoArgsConstructor
 @AllArgsConstructor
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class FilterRequest {
