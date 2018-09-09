4fff1357ae81982d16437488de13e38ba1768faa
Sebastian Pacuk <spacuk@atlassian.com>
2018-09-09 13:30:31 +0200

    Added token service

--- backend/src/main/java/com/pfm/auth/TokenService.java
+++ backend/src/main/java/com/pfm/auth/TokenService.java
@@ -1,8 +1,36 @@
 package com.pfm.auth;
 
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.UUID;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@AllArgsConstructor
 public class TokenService {
+
+  private static HashMap<String, LocalDateTime> tokens = new HashMap<>();
+
+  public String generateToken() {
+    UUID uuid = UUID.randomUUID();
+    String token = uuid.toString();
+    tokens.put(token, LocalDateTime.now());
+    return token;
+  }
+
+  public boolean validateToken(String token) {
+    LocalDateTime creationDate = tokens.get(token);
+
+    if (creationDate == null) {
+      return false;
+    }
+
+    if (!creationDate.plusMinutes(15).isAfter(LocalDateTime.now())) {
+      return false;
+    }
+
+    return true;
+  }
 
 }
