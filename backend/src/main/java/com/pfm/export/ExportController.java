0247c191f27998de751e3ce5a1407d5762c8625c
piotr.kolacz <piotr.kolacz@jivesoftware.com>
2018-09-25 21:20:53 +0200

    Removed unnecessary transaction ids

--- backend/src/main/java/com/pfm/export/ExportController.java
+++ backend/src/main/java/com/pfm/export/ExportController.java
@@ -42,6 +42,7 @@ public class ExportController implements ExportApi {
 
       monthToTransactionMap.computeIfAbsent(key, k -> new ArrayList<>());
 
+      transaction.setId(null);
       monthToTransactionMap.get(key).add(transaction);
     }
 
