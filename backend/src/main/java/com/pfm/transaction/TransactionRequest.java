b3f63960fd88c3d0e636d4c63b0aae13b9254fbf
piotr.kolacz <piotr.kolacz@jivesoftware.com>
2018-10-12 21:04:12 +0200

    Export - line coverage 100%.

--- backend/src/main/java/com/pfm/transaction/TransactionRequest.java
+++ backend/src/main/java/com/pfm/transaction/TransactionRequest.java
@@ -3,6 +3,7 @@ package com.pfm.transaction;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import io.swagger.annotations.ApiModelProperty;
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -11,25 +12,23 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
+@Setter
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class TransactionRequest {
 
-  @Setter
   @ApiModelProperty(value = "Description", required = true, example = "Cinema - Star Wars 5")
   protected String description;
 
-  @Setter
   @ApiModelProperty(value = "Category id", required = true, example = "1")
   protected Long categoryId;
 
-  @Setter
   @ApiModelProperty(value = "Date", required = true, example = "2018-12-31")
   protected LocalDate date;
 
   @ApiModelProperty(value = "Price & Account entries")
-  private List<AccountPriceEntry> accountPriceEntries;
+  private List<AccountPriceEntry> accountPriceEntries = new ArrayList<>();
 
 }
