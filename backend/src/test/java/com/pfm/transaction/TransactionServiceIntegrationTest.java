f27ccf9d6b1d8e11bb9ae19e19490378f07485c8
Sebastian Pacuk <spacuk@atlassian.com>
2018-10-17 22:06:37 +0200

    added test for delete method

--- backend/src/test/java/com/pfm/transaction/TransactionServiceIntegrationTest.java
+++ backend/src/test/java/com/pfm/transaction/TransactionServiceIntegrationTest.java
@@ -1,5 +1,6 @@
 package com.pfm.transaction;
 
+import static com.pfm.test.helpers.TestCategoryProvider.categoryCar;
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertThat;
@@ -7,25 +8,33 @@ import static org.junit.Assert.fail;
 
 import com.pfm.account.Account;
 import com.pfm.account.AccountService;
+import com.pfm.category.CategoryService;
 import java.math.BigDecimal;
+import java.time.LocalDate;
 import java.util.Collections;
 import java.util.Optional;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.SpyBean;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class TransactionServiceIntegrationTest {
 
-  @Autowired
+  @SpyBean
   private TransactionService transactionService;
 
   @Autowired
   private AccountService accountService;
+
+  @Autowired
+  private CategoryService categoryService;
 
   @Test
   public void shouldRollbackAccountStateUpdateWhenSaveFailsBecauseOfInvalidData() {
@@ -59,4 +68,44 @@ public class TransactionServiceIntegrationTest {
     assertThat(account.getBalance(), is(BigDecimal.valueOf(1000, 2)));
   }
 
+  @Test
+  public void shouldRollbackAccountStateUpdateWhenDeleteFailsBecauseOfInvalidData() {
+    // given
+    Account savedAccount = accountService.addAccount(Account.builder()
+        .balance(BigDecimal.TEN)
+        .name("mBank")
+        .build()
+    );
+
+    long categoryId = categoryService.addCategory(categoryCar()).getId();
+
+    Transaction transaction = new Transaction();
+    transaction.setAccountPriceEntries(Collections.singletonList(AccountPriceEntry.builder()
+        .accountId(savedAccount.getId())
+        .price(BigDecimal.ONE)
+        .build())
+    );
+    transaction.setCategoryId(categoryId);
+    transaction.setDescription("Transaction with price 1");
+    transaction.setDate(LocalDate.now());
+
+    transactionService.addTransaction(transaction);
+    Mockito.when(transactionService.getTransactionById(42)).thenReturn(Optional.of(transaction));
+    // when
+
+    try {
+      transactionService.deleteTransaction(42);
+      fail();
+    } catch (EmptyResultDataAccessException ex) {
+      assertNotNull(ex); // just not to leave empty
+    }
+
+    // then
+    Optional<Account> returnedAccount = accountService.getAccountById(savedAccount.getId());
+    assertThat(returnedAccount.isPresent(), is(true));
+
+    Account account = returnedAccount.get();
+    assertThat(returnedAccount.get().getBalance(), is(BigDecimal.valueOf(1100, 2)));
+  }
+  
 }
