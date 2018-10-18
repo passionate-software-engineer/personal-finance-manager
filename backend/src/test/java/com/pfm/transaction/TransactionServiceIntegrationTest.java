24d00f1217e67260782d33d1cee8fdd46126b1c6
Sebastian Pacuk <spacuk@atlassian.com>
2018-10-18 21:41:59 +0200

    added test for update method

--- backend/src/test/java/com/pfm/transaction/TransactionServiceIntegrationTest.java
+++ backend/src/test/java/com/pfm/transaction/TransactionServiceIntegrationTest.java
@@ -1,6 +1,10 @@
 package com.pfm.transaction;
 
-import static com.pfm.test.helpers.TestCategoryProvider.categoryCar;
+import static com.pfm.helpers.TestCategoryProvider.categoryCar;
+import static com.pfm.helpers.TestCategoryProvider.categoryOil;
+import static com.pfm.helpers.TestUsersProvider.userMarian;
+import static com.pfm.helpers.TestUsersProvider.userMirek;
+import static com.pfm.helpers.TestUsersProvider.userZdzislaw;
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertThat;
@@ -8,6 +12,7 @@ import static org.junit.Assert.fail;
 
 import com.pfm.account.Account;
 import com.pfm.account.AccountService;
+import com.pfm.auth.UserService;
 import com.pfm.category.CategoryService;
 import java.math.BigDecimal;
 import java.time.LocalDate;
@@ -36,12 +41,19 @@ public class TransactionServiceIntegrationTest {
   @Autowired
   private CategoryService categoryService;
 
+  @Autowired
+  UserService userService;
+
   @Test
   public void shouldRollbackAccountStateUpdateWhenSaveFailsBecauseOfInvalidData() {
+
     // given
+    long userId = userService.registerUser(userZdzislaw()).getId();
+
     Account savedAccount = accountService.addAccount(Account.builder()
         .balance(BigDecimal.TEN)
-        .name("mBank")
+        .name("Idea bank")
+        .userId(userId)
         .build()
     );
 
@@ -54,14 +66,14 @@ public class TransactionServiceIntegrationTest {
 
     // when
     try {
-      transactionService.addTransaction(transaction);
+      transactionService.addTransaction(userId, transaction);
       fail();
     } catch (DataIntegrityViolationException ex) {
       assertNotNull(ex); // just not to leave empty
     }
 
     // then
-    Optional<Account> returnedAccount = accountService.getAccountById(savedAccount.getId());
+    Optional<Account> returnedAccount = accountService.getAccountByIdAndUserId(savedAccount.getId(), userId);
     assertThat(returnedAccount.isPresent(), is(true));
 
     Account account = returnedAccount.get();
@@ -70,14 +82,18 @@ public class TransactionServiceIntegrationTest {
 
   @Test
   public void shouldRollbackAccountStateUpdateWhenDeleteFailsBecauseOfInvalidData() {
+
     // given
+    long userId = userService.registerUser(userMarian()).getId();
+
     Account savedAccount = accountService.addAccount(Account.builder()
         .balance(BigDecimal.TEN)
         .name("mBank")
+        .userId(userId)
         .build()
     );
 
-    long categoryId = categoryService.addCategory(categoryCar()).getId();
+    long categoryId = categoryService.addCategory(categoryCar(), userId).getId();
 
     Transaction transaction = new Transaction();
     transaction.setAccountPriceEntries(Collections.singletonList(AccountPriceEntry.builder()
@@ -89,23 +105,71 @@ public class TransactionServiceIntegrationTest {
     transaction.setDescription("Transaction with price 1");
     transaction.setDate(LocalDate.now());
 
-    transactionService.addTransaction(transaction);
-    Mockito.when(transactionService.getTransactionById(42)).thenReturn(Optional.of(transaction));
+    transactionService.addTransaction(userId, transaction);
+    Mockito.when(transactionService.getTransactionByIdAndUserId(42, userId)).thenReturn(Optional.of(transaction));
     // when
 
     try {
-      transactionService.deleteTransaction(42);
+      transactionService.deleteTransaction(42, userId);
       fail();
     } catch (EmptyResultDataAccessException ex) {
       assertNotNull(ex); // just not to leave empty
     }
 
     // then
-    Optional<Account> returnedAccount = accountService.getAccountById(savedAccount.getId());
-    assertThat(returnedAccount.isPresent(), is(true));
+    Optional<Account> returnedAccount = accountService.getAccountByIdAndUserId(savedAccount.getId(), userId);
 
-    Account account = returnedAccount.get();
+    assertThat(returnedAccount.isPresent(), is(true));
     assertThat(returnedAccount.get().getBalance(), is(BigDecimal.valueOf(1100, 2)));
   }
-  
+
+  @Test
+  public void shouldRollbackAccountStateUpdateWhenUpdateFailsBecauseOfInvalidData() {
+
+    // given
+    long userId = userService.registerUser(userMirek()).getId();
+
+    Account savedAccount = accountService.addAccount(Account.builder()
+        .balance(BigDecimal.TEN)
+        .name("Ing")
+        .userId(userId)
+        .build()
+    );
+
+    long categoryId = categoryService.addCategory(categoryOil(), userId).getId();
+
+    Transaction transaction = new Transaction();
+    transaction.setAccountPriceEntries(Collections.singletonList(AccountPriceEntry.builder()
+        .accountId(savedAccount.getId())
+        .price(BigDecimal.ONE)
+        .build())
+    );
+    transaction.setCategoryId(categoryId);
+    transaction.setDescription("Trans with price 1");
+    transaction.setDate(LocalDate.now());
+    transaction.setUserId(userId);
+
+    long transactionId = transactionService.addTransaction(userId, transaction).getId();
+
+    transaction.setAccountPriceEntries(Collections.singletonList(AccountPriceEntry.builder()
+        .accountId(savedAccount.getId())
+        .price(BigDecimal.TEN)
+        .build()));
+    transaction.setDate(null);
+
+    // when
+    try {
+      transactionService.updateTransaction(transactionId, userId, transaction);
+      fail();
+    } catch (UnsupportedOperationException ex) {
+      assertNotNull(ex); // just not to leave empty
+    }
+
+    // then
+    Optional<Account> returnedAccount = accountService.getAccountByIdAndUserId(savedAccount.getId(), userId);
+
+    assertThat(returnedAccount.isPresent(), is(true));
+    assertThat(returnedAccount.get().getBalance(), is(BigDecimal.valueOf(1100, 2)));
+  }
+
 }
