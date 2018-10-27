8a05e7f0a23012af79ed21fb61260506c3cedf8a
piotr.kolacz <piotr.kolacz@jivesoftware.com>
2018-10-27 23:25:16 +0200

    Extracted startup script to file

--- start_backend.sh
+++ start_backend.sh
@@ -3,18 +3,23 @@
 set -x
 set -e
 
+DATE=$(date '+%Y-%m-%d-%H:%M')
+
 OLD_PROCESS_ID=$(ps -ef | grep "[b]ackend-1.0.jar" | awk '{print $2}')
 if [ -n "$OLD_PROCESS_ID" ]
 then
   kill $OLD_PROCESS_ID
+  sleep 10
 else
   echo "Process was already stopped, no need to stop it again"
 fi
 
 if [ -f backend-1.0.jar.new ]; then
   chmod 700 backend-1.0.jar.new
-  mv --force backend-1.0.jar backend-1.0.jar.bak
+  mv --force backend-1.0.jar backend-1.0.jar.$DATE
   mv --force backend-1.0.jar.new backend-1.0.jar
 fi
+
+mv application.log > application.log.$DATE
 
 nohup java -jar backend-1.0.jar --spring.profiles.active=aws >> application.log 2>> application.log &
