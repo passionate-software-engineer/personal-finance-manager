5e9a604849ee5a6ced113f5cd582eeb470fea15f
SebaPacuk <spacuk@gmail.com>
2018-10-30 22:39:33 +0100

    working on history - unify approach, changes in appearance.

--- backend/src/main/java/com/pfm/account/Account.java
+++ backend/src/main/java/com/pfm/account/Account.java
@@ -43,11 +43,11 @@ public final class Account implements DifferenceProvider<Account> {
     List<String> differences = new ArrayList<>();
 
     if (!(this.getName().equals(otherAccount.getName()))) {
-      differences.add(String.format(UPDATE_ENTRY_TEMPLATE, "name", this.getName(), otherAccount.getName()));
+      differences.add(String.format(UPDATE_ENTRY_TEMPLATE, "name ", this.getName(), otherAccount.getName()));
     }
 
     if (!(this.getBalance().compareTo(otherAccount.getBalance()) == 0)) {
-      differences.add(String.format(UPDATE_ENTRY_TEMPLATE, "balance", this.getBalance().toString(),
+      differences.add(String.format(UPDATE_ENTRY_TEMPLATE, "balance ", this.getBalance().toString(),
           convertBigDecimalToString(otherAccount.getBalance())));
     }
 
