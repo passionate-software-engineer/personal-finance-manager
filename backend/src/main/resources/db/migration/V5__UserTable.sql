5a8ef845ee5fdf6df031d5c0780e3e195d375663
Mateusz Gorski <mateuszgorski@gmail.com>
2018-11-12 16:54:07 +0100

    changed column size in flyway - fixed 401 on too  long usernames

--- backend/src/main/resources/db/migration/V5__UserTable.sql
+++ backend/src/main/resources/db/migration/V5__UserTable.sql
@@ -1,10 +1,10 @@
 CREATE TABLE app_user
 (
   id         BIGSERIAL PRIMARY KEY,
-  username   VARCHAR(255) NOT NULL UNIQUE,
-  password   VARCHAR(255) NOT NULL,
-  first_name VARCHAR(255) NOT NULL,
-  last_name  VARCHAR(255) NOT NULL
-);
+  username   VARCHAR(600) NOT NULL UNIQUE,
+  password   VARCHAR(600) NOT NULL,
+  first_name VARCHAR(600) NOT NULL,
+  last_name  VARCHAR(600) NOT NULL
+);g
 
 
