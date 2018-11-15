42e8f61db0f09b776fa9aa5106575e78628a6aae
Mateusz Gorski <mateuszgorski@gmail.com>
2018-11-15 13:57:11 +0100

    changed message properties for en and pl

--- backend/src/main/java/com/pfm/config/MessagesProvider.java
+++ backend/src/main/java/com/pfm/config/MessagesProvider.java
@@ -46,10 +46,10 @@ public class MessagesProvider {
   public static final String USERNAME_CONTAINS_WHITSPACE = "userValidator.usernameContainsWhitespaces";
   public static final String PASSWORD_CONTAINS_WHITSPACE = "userValidator.passwordContainsWhitespaces";
   public static final String USERNAME_OR_PASSWORD_IS_INCORRECT = "userController.usernameOrPassowrdIsIncorrect";
-  public static final String TOO_LONG_USERNAME = "Username can be 20 characters max";
-  public static final String TOO_LONG_PASSWORD = "Password can be 20 characters max ";
-  public static final String TOO_LONG_FIRST_NAME = "First name can be 20 characters max";
-  public static final String TOO_LONG_LAST_NAME = "Last name can be 20 characters max";
+  public static final String TOO_LONG_USERNAME = "userValidator.tooLongUsername";
+  public static final String TOO_LONG_PASSWORD = "userValidator.tooLongPassword";
+  public static final String TOO_LONG_FIRST_NAME = "userValidator.tooLongFirstName";
+  public static final String TOO_LONG_LAST_NAME = "userValidator.tooLongLastName";
 
   public static final String MAIN_CATEGORY = "historyInfoProvider.mainCategory";
 
