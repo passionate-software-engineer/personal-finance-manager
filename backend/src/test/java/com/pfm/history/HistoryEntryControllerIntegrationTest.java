8a45f0edf2c55e7161376b59a26536a9e683642d
Paweł <win_dows@o2.pl>
2019-04-01 21:03:43 +0200

    Added comment

--- backend/src/test/java/com/pfm/history/HistoryEntryControllerIntegrationTest.java
+++ backend/src/test/java/com/pfm/history/HistoryEntryControllerIntegrationTest.java
@@ -102,6 +102,8 @@ class HistoryEntryControllerIntegrationTest extends IntegrationTestsBase {
     //then
     List<HistoryInfo> historyInfosExpected = new ArrayList<>();
 
+    // comment for push
+
     historyInfosExpected.add(HistoryInfo.builder()
         .id(3L)
         .name("name")
