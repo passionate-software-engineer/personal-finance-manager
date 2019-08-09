18e1876f75898e99585bd0f089e7975d05629ecf
Lukasz <lukmyrta@gmail.com>
2019-08-09 11:23:44 +0200

    table transaction constraints changed from NOT nULL   to   NULL

--- backend/src/main/resources/db/migration/V2__TransactionTable.sql
+++ backend/src/main/resources/db/migration/V2__TransactionTable.sql
@@ -1,9 +1,9 @@
 CREATE TABLE transaction
 (
   id          BIGSERIAL PRIMARY KEY,
-  description VARCHAR(255)   NOT NULL,
+  description VARCHAR(255)    NULL,
   category_id BIGINT REFERENCES category (id),
   account_id  BIGINT REFERENCES account (id),
-  price       NUMERIC(19, 2) NOT NULL,
-  date        DATE           NOT NULL
+  price       NUMERIC(19, 2)  NULL,
+  date        DATE            NULL
 );
