d0f954b5ba5a810103fb66a4d19ad7481fcdd631
Lukasz <lukmyrta@gmail.com>
2019-10-07 14:36:45 +0200

    ADD: CommitResponseBody class CHANGE: commit method

--- backend/src/main/java/com/pfm/transaction/TransactionApi.java
+++ backend/src/main/java/com/pfm/transaction/TransactionApi.java
@@ -31,7 +31,7 @@ public interface TransactionApi {
 
   @ApiOperation(value = "Create a new transaction", response = long.class, authorizations = {@Authorization(value = "Bearer")})
   @PostMapping
-  ResponseEntity<?> addTransaction(TransactionRequest transactionRequest);
+  ResponseEntity<?> addTransaction(TransactionRequest transactionRequest, boolean shouldReturnTransaction);
 
   @ApiOperation(value = "Update an existing transaction", response = Void.class, authorizations = {@Authorization(value = "Bearer")})
   @PutMapping(value = "/{transactionId}")
