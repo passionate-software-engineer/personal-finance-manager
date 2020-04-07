6d934add8cfe96d42ab60c12057118b49dc787e4
BarbaraMielczarek <46875769+BarbaraMielczarek@users.noreply.github.com>
2020-04-08 00:11:09 +0200

    updated AccountTypeApi

---
+++ backend/src/test/java/com/pfm/type/AccountTypeControllerTransactionalTest.java
@@ -0,0 +1,65 @@
+package com.pfm.type;
+
+import static com.pfm.helpers.TestAccountTypeProvider.accountInvestment;
+import static com.pfm.helpers.TestUsersProvider.userZdzislaw;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.hasSize;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.fail;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.when;
+
+import com.pfm.account.type.AccountType;
+import com.pfm.account.type.AccountTypeController;
+import com.pfm.account.type.AccountTypeService;
+import com.pfm.auth.UserProvider;
+import com.pfm.helpers.IntegrationTestsBase;
+import com.pfm.history.HistoryEntryService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.mock.mockito.SpyBean;
+
+class AccountTypeControllerTransactionalTest extends IntegrationTestsBase {
+
+  @SpyBean
+  private HistoryEntryService historyEntryService;
+
+  @MockBean
+  private UserProvider userProvider;
+
+  @SpyBean
+  private AccountTypeService accountTypeService;
+
+  @Autowired
+  private AccountTypeController accountTypeController;
+
+  @Override
+  @BeforeEach
+  public void before() {
+    super.before();
+    userId = userService.registerUser(userZdzislaw()).getId();
+    when(userProvider.getCurrentUserId()).thenReturn(userId);
+  }
+
+  @Test
+  void shouldRollbackTransactionWhenAccountTypeAddFailed() {
+    //given
+    AccountType accountType = accountInvestment();
+    doThrow(IllegalStateException.class).when(historyEntryService).addHistoryEntryOnAdd(any(Object.class), any(Long.class));
+
+    // when
+    try {
+      accountTypeController.addAccountType(convertAccountTypeToAccountTypeRequest(accountType));
+      fail();
+    } catch (IllegalStateException ex) {
+      assertNotNull(ex);
+    }
+
+    //then
+    assertThat(accountTypeService.getAccountTypes(userId), hasSize(0));
+  }
+
+}
