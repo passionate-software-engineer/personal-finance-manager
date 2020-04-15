32005ba5bc9521b3f917440097665800182ed698
passionate-software-engineer <45860871+passionate-software-engineer@users.noreply.github.com>
2020-04-15 19:11:40 +0200

    Update run_e2e.sh

--- scripts/run_e2e.sh
+++ scripts/run_e2e.sh
@@ -3,7 +3,7 @@
 trap 'echo "ERROR: $BASH_SOURCE:$LINENO $BASH_COMMAND" >&2' ERR
 
 BACKEND_PORT=$(( ( RANDOM % 5000 )  + 40000 ))
-FRONTEND_PORT=$(( ( RANDOM % 5000 )  + 46000 ))
+# FRONTEND_PORT=$(( ( RANDOM % 5000 )  + 46000 ))
 
 rm -f output.log
 java -jar backend/build/libs/backend-1.0.jar --server.port=$BACKEND_PORT >>output.log 2>&1 &
@@ -25,7 +25,17 @@ cd frontend
 sed -i "s/8088/$BACKEND_PORT/g" src/environments/environment.ts
 cat src/environments/environment.ts
 
-ng e2e --port $FRONTEND_PORT
+rm -f frontend_output.log
+ng serve >>frontend_output.log 2>&1 &
+
+tail -f frontend_output.log | while read LOG_LINE
+do
+   echo "${LOG_LINE}"
+   [[ "${LOG_LINE}" == *"Angular Live Development Server is listening on localhost:4200"* ]] && pkill -P $$ tail && echo "Frontend started successfully"
+   [[ "${LOG_LINE}" == *"ERROR in"* ]] && pkill -P $$ tail && echo "Frontend failed to start" && exit 500
+done
+
+protractor e2e/protractor.conf.js
 
 echo "Stopping application with pid=$backend_pid"
 kill $backend_pid
