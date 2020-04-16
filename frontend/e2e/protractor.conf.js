72d1e63df326832b9d8b4a632a8f372241c24307
piotr.kolacz <piotr.kolacz@jivesoftware.com>
2020-04-16 10:44:05 +0200

    Fixed tests

--- frontend/e2e/protractor.conf.js
+++ frontend/e2e/protractor.conf.js
@@ -12,16 +12,16 @@ exports.config = {
   capabilities: {
     'browserName': 'chrome',
 
-    // allows different specs to run in parallel.
-    // If this is set to be true, specs will be sharded by file
-    // (i.e. all files to be run by this set of capabilities will run in parallel).
-    // Default is false.
-    shardTestFiles: true,
-
-    // Maximum number of browser instances that can run in parallel for this
-    // set of capabilities. This is only needed if shardTestFiles is true.
-    // Default is 1.
-    maxInstances: 2,
+//    // allows different specs to run in parallel.
+//    // If this is set to be true, specs will be sharded by file
+//    // (i.e. all files to be run by this set of capabilities will run in parallel).
+//    // Default is false.
+//    shardTestFiles: true,
+//
+//    // Maximum number of browser instances that can run in parallel for this
+//    // set of capabilities. This is only needed if shardTestFiles is true.
+//    // Default is 1.
+//    maxInstances: 2,
 
     chromeOptions: {
       args: ['--headless', '--disable-gpu', '--start-maximized', '--no-sandbox']
