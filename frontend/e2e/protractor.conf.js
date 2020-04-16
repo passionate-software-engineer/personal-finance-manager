f603434844736acf4b189f5209098c7479cb394a
passionate-software-engineer <45860871+passionate-software-engineer@users.noreply.github.com>
2020-04-16 11:55:27 +0200

    Added test retry functionality (#256)
    * Added test retry functionality
    
    * Update nodejs.yml
    
    * Update nodejs.yml
    
    * Update nodejs.yml
    
    * Update nodejs.yml
    
    * Update run_e2e.sh
    
    * Update run_e2e.sh
    
    * Update run_e2e.sh
    
    * Fixed tests
    
    * Fixed tests
    
    * Update nodejs.yml
    
    * Update nodejs.yml
    
    * Fixed tests
    
    * Update nodejs.yml
    
    Co-authored-by: piotr.kolacz <piotr.kolacz@jivesoftware.com>

--- frontend/e2e/protractor.conf.js
+++ frontend/e2e/protractor.conf.js
@@ -2,6 +2,7 @@
 // https://github.com/angular/protractor/blob/master/lib/config.ts
 
 const {SpecReporter} = require('jasmine-spec-reporter');
+const retry = require('protractor-retry').retry;
 
 exports.config = {
   allScriptsTimeout: 20000,
@@ -11,10 +12,22 @@ exports.config = {
   capabilities: {
     'browserName': 'chrome',
 
+//    // allows different specs to run in parallel.
+//    // If this is set to be true, specs will be sharded by file
+//    // (i.e. all files to be run by this set of capabilities will run in parallel).
+//    // Default is false.
+//    shardTestFiles: true,
+//
+//    // Maximum number of browser instances that can run in parallel for this
+//    // set of capabilities. This is only needed if shardTestFiles is true.
+//    // Default is 1.
+//    maxInstances: 2,
+
     chromeOptions: {
       args: ['--headless', '--disable-gpu', '--start-maximized', '--no-sandbox']
     }
   },
+
   directConnect: true,
   baseUrl: 'http://localhost:4200/',
   framework: 'jasmine',
@@ -24,12 +37,24 @@ exports.config = {
     print: function () {
     }
   },
+
+
+
   onPrepare() {
+    retry.onPrepare();
     require('ts-node').register({
       project: require('path').join(__dirname, './tsconfig.e2e.json')
     });
     jasmine.getEnv().addReporter(
       new SpecReporter({spec: {displayStacktrace: true}}));
+  },
+
+  onCleanUp : function(results) {
+    retry.onCleanUp(results);
+  },
+
+  afterLaunch : function() {
+    return retry.afterLaunch(3);
   }
 };
 
--- scripts/run_e2e.sh
+++ scripts/run_e2e.sh
@@ -3,7 +3,7 @@
 trap 'echo "ERROR: $BASH_SOURCE:$LINENO $BASH_COMMAND" >&2' ERR
 
 BACKEND_PORT=$(( ( RANDOM % 5000 )  + 40000 ))
-FRONTEND_PORT=$(( ( RANDOM % 5000 )  + 46000 ))
+# FRONTEND_PORT=$(( ( RANDOM % 5000 )  + 46000 ))
 
 rm -f output.log
 java -jar backend/build/libs/backend-1.0.jar --server.port=$BACKEND_PORT >>output.log 2>&1 &
@@ -18,14 +18,28 @@ do
    [[ "${LOG_LINE}" == *"APPLICATION FAILED TO START"* ]] && pkill -P $$ tail && echo "Application failed to start" && exit 500
 done
 
-echo "Starting E2E tests"
+echo "Starting frontend"
 
-set -e
 cd frontend
 sed -i "s/8088/$BACKEND_PORT/g" src/environments/environment.ts
 cat src/environments/environment.ts
 
-ng e2e --port $FRONTEND_PORT
+rm -f frontend_output.log
+touch frontend_output.log
+ng serve >>frontend_output.log 2>&1 &
+
+tail -f frontend_output.log | while read LOG_LINE
+do
+   echo "${LOG_LINE}"
+   [[ "${LOG_LINE}" == *"Angular Live Development Server is listening on localhost:4200"* ]] && pkill -P $$ tail && echo "Frontend started successfully"
+   [[ "${LOG_LINE}" == *"ERROR in"* ]] && pkill -P $$ tail && echo "Frontend failed to start" && exit 500
+done
+
+echo "Starting E2E"
+set -e
+
+webdriver-manager update
+protractor e2e/protractor.conf.js
 
 echo "Stopping application with pid=$backend_pid"
 kill $backend_pid
