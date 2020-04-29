b01f13fb7b8a99f5fadcf08e25f6ce7f74162c73
passionate-software-engineer <45860871+passionate-software-engineer@users.noreply.github.com>
2020-04-29 13:33:58 +0200

    Update checkstyle.xml (#261)
    * Update checkstyle.xml
    
    * Corrected formatting.
    
    Co-authored-by: piotr.kolacz <piotr.kolacz@jivesoftware.com>

--- backend/src/main/java/com/pfm/config/SwaggerConfig.java
+++ backend/src/main/java/com/pfm/config/SwaggerConfig.java
@@ -10,6 +10,7 @@ import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.ApiKey;
 import springfox.documentation.service.Contact;
+import springfox.documentation.service.Tag;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -26,6 +27,7 @@ public class SwaggerConfig {
         .apis(RequestHandlerSelectors.basePackage("com.pfm"))
         .paths(any())
         .build()
+        .tags(new Tag("account-controller", "Controller used to list / add / update / delete accounts."))
         .securitySchemes(Collections.singletonList(new ApiKey("Bearer", "Authorization", "header")));
   }
 
