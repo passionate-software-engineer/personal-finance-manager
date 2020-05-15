fb6f05fdc909f1ef712363a44b50d0d9a4797727
Lukasz <lukmyrta@gmail.com>
2020-05-15 16:26:30 +0200

    changes applied

--- backend/src/main/java/com/pfm/transaction/import1/ParsedTransactionPropertyFilter.java
+++ backend/src/main/java/com/pfm/transaction/import1/ParsedTransactionPropertyFilter.java
@@ -3,6 +3,7 @@ package com.pfm.transaction.import1;
 import java.util.function.Predicate;
 
 public interface ParsedTransactionPropertyFilter {
+
   static Predicate<String> notEmpty() {
     return stringProperty -> !stringProperty.isEmpty();
   }
