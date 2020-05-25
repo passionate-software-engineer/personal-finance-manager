31430926d381dee24fb8a7e0594a190376ea9dca
micho997 <61161334+micho997@users.noreply.github.com>
2020-05-25 10:54:27 +0200

    Validate import Data - Michał Zięba

--- backend/src/main/java/com/pfm/export/validation/ImportCategoryValidator.java
+++ backend/src/main/java/com/pfm/export/validation/ImportCategoryValidator.java
@@ -10,7 +10,7 @@ public class ImportCategoryValidator {
 
   private static final String CATEGORY_NAME_MISSING = "Category name is missing";
   private static final String PARENT_CATEGORY_NAME_MISSING = " category has missing parent category name";
-  private static final String PRIORITY_MISSING = " category has missing priority";
+  private static final String PRIORITY_MISSING = " category has incorrect priority";
 
   List<String> validate(List<ExportResult.ExportCategory> inputData) {
 
--- backend/src/test/java/com/pfm/export/validation/ImportCategoryValidatorTest.java
+++ backend/src/test/java/com/pfm/export/validation/ImportCategoryValidatorTest.java
@@ -39,12 +39,12 @@ class ImportCategoryValidatorTest {
         Arguments.arguments(missingParentCategoryName(),
             Collections.singletonList("CategoryName category has missing parent category name")),
 
-        Arguments.arguments(missingPriority(),
-            Collections.singletonList("CategoryName category has missing priority")),
+        Arguments.arguments(incorrectPriority(),
+            Collections.singletonList("CategoryName category has incorrect priority")),
 
         Arguments.arguments(onlyName(),
             Arrays.asList("CategoryName category has missing parent category name",
-                "CategoryName category has missing priority")),
+                "CategoryName category has incorrect priority")),
 
         Arguments.arguments(missingAllData(),
             Collections.singletonList("Category name is missing"))
@@ -53,34 +53,38 @@ class ImportCategoryValidatorTest {
   }
 
   private static ExportResult.ExportCategory missingName() {
-    return ExportCategory.builder()
-        .parentCategoryName("ParentCategoryName")
-        .priority(100)
-        .build();
+    ExportCategory exportCategory = correctCategory();
+    exportCategory.setName("");
+    return exportCategory;
   }
 
   private static ExportResult.ExportCategory missingParentCategoryName() {
-    return ExportCategory.builder()
-        .name("CategoryName")
-        .priority(100)
-        .build();
+    ExportCategory exportCategory = correctCategory();
+    exportCategory.setParentCategoryName("");
+    return exportCategory;
   }
 
-  private static ExportResult.ExportCategory missingPriority() {
-    return ExportCategory.builder()
-        .name("CategoryName")
-        .parentCategoryName("ParentCategoryName")
-        .build();
+  private static ExportResult.ExportCategory incorrectPriority() {
+    ExportCategory exportCategory = correctCategory();
+    exportCategory.setPriority(9999999);
+    return exportCategory;
   }
 
   private static ExportResult.ExportCategory missingAllData() {
-    return ExportCategory.builder()
-        .build();
+    return new ExportCategory();
   }
 
   private static ExportResult.ExportCategory onlyName() {
     return ExportCategory.builder()
         .name("CategoryName")
+        .build();
+  }
+
+  private static ExportResult.ExportCategory correctCategory() {
+    return ExportCategory.builder()
+        .name("CategoryName")
+        .parentCategoryName("ParentCategoryName")
+        .priority(100)
         .build();
   }
 }
