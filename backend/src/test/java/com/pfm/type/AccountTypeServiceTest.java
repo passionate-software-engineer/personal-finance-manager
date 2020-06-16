a888f101883beaa32f07dc1e2d31850eb5ee0347
BarbaraMielczarek <46875769+BarbaraMielczarek@users.noreply.github.com>
2020-06-16 23:02:24 +0200

    created shouldGetAllAccountTypes Test

--- backend/src/test/java/com/pfm/type/AccountTypeServiceTest.java
+++ backend/src/test/java/com/pfm/type/AccountTypeServiceTest.java
@@ -2,10 +2,12 @@ package com.pfm.type;
 
 import static com.pfm.config.MessagesProvider.ACCOUNT_TYPE_ID_DOES_NOT_EXIST;
 import static com.pfm.config.MessagesProvider.getMessage;
+import static com.pfm.helpers.TestAccountTypeProvider.accountCredit;
 import static com.pfm.helpers.TestAccountTypeProvider.accountInvestment;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.lessThan;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.when;
@@ -13,6 +15,8 @@ import static org.mockito.Mockito.when;
 import com.pfm.account.type.AccountType;
 import com.pfm.account.type.AccountTypeRepository;
 import com.pfm.account.type.AccountTypeService;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -83,6 +87,34 @@ class AccountTypeServiceTest {
 
     // then
     assertThat(exception.getMessage(), is("Account type with id: " + id + " does not exist in database"));
+  }
+
+  @Test
+  public void shouldGetAllAccountTypes() {
+    // given
+    AccountType accountTypeCredit = accountCredit();
+    accountTypeCredit.setId(1L);
+    AccountType accountTypeInvestment = accountInvestment();
+    accountTypeInvestment.setId(2L);
+
+    when(accountTypeRepository.findByUserId(MOCK_USER_ID)).thenReturn(Arrays.asList(accountTypeCredit, accountTypeInvestment));
+
+    // when
+    List<AccountType> actualAccountTypesList = accountTypeService.getAccountTypes(MOCK_USER_ID);
+
+    // then
+    assertThat(actualAccountTypesList.size(), is(2));
+
+    // account types should be sorted by id
+    assertThat(accountTypeCredit.getId(), lessThan(accountTypeInvestment.getId()));
+
+    AccountType accountType1 = actualAccountTypesList.get(0);
+    assertThat(accountType1.getId(), is(equalTo(accountTypeCredit.getId())));
+    assertThat(accountType1.getName(), is(equalTo(accountTypeCredit.getName())));
+
+    AccountType accountType2 = actualAccountTypesList.get(1);
+    assertThat(accountType2.getId(), is(equalTo(accountTypeInvestment.getId())));
+    assertThat(accountType2.getName(), is(equalTo(accountTypeInvestment.getName())));
   }
 
 }
